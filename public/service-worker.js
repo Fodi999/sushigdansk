1102972d02eb02797a2b79ee514b803e377095b9
Dmytro Fomin <78318568+Fodi999@users.noreply.github.com>
2024-07-30 19:29:46 +0200

    header fix

---
+++ public/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'sushi-delivery-cache-v1';
+const urlsToCache = [
+  '/',
+  '/css/app.css',
+  '/js/main.js',
+  '/images/apple-touch-icon.png',
+  '/images/favicon-32x32.png',
+  '/images/favicon-16x16.png',
+  '/images/android-chrome-192x192.png',
+  '/images/android-chrome-512x512.png',
+  '/images/safari-pinned-tab.svg',
+  '/images/Rectangle-3.webp'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+ 
